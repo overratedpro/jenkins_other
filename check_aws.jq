f8badb53ef9a60966280b1e64c73258d1699963a
overratedpro <github@rezult.at>
2017-12-14 19:29:10 +0100

    merge into a single script file

--- check_aws.jq
+++ check_aws.jq
@@ -1,9 +1,1 @@
-map(.InstanceStatuses) |
-add |
-map( select(
-    ( .SystemStatus.Status | test("^ok$") | not ) or
-    ( .InstanceStatus.Status | test("^ok$") | not )
-)? ) |
-map({"key": .AvailabilityZone, "value": .InstanceId}) |
-group_by(.key) |
-map({(.[0].key): (map(.value) | join(", "))})
+# REMOVED, please see check_aws.sh
--- check_aws.sh
+++ check_aws.sh
@@ -9,11 +9,23 @@
 # 2) aws config and credentials setup in ~/.aws
 #
 
+read -d '' JQ_TRANSFORMATION <<- 'EOF' || :
+map(.InstanceStatuses) |
+add |
+map( select(
+    ( .SystemStatus.Status | test("^ok$") | not ) or
+    ( .InstanceStatus.Status | test("^ok$") | not )
+)? ) |
+map({"key": .AvailabilityZone, "value": .InstanceId}) |
+group_by(.key) |
+map({(.[0].key): (map(.value) | join(", "))})
+EOF
+
 AWS_INSTANCE_STATUS="$( aws ec2 describe-regions --output json |\
     jq --compact-output '.Regions | .[] | .RegionName' |\
     xargs -r -n1 -- aws ec2 describe-instance-status --output json --region |\
     jq --slurp --compact-output --monochrome-output \
-        --from-file "$(dirname "${BASH_SOURCE[0]}")/check_aws.jq" )"
+        --from-file <(echo "${JQ_TRANSFORMATION}") )"
 
 if [ $(echo "${AWS_INSTANCE_STATUS}" | jq --raw-output 'length') -gt 0 ]; then
     echo 'EC2 instance checks failed: '"${AWS_INSTANCE_STATUS}"
